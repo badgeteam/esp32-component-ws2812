c1c750d2ce0a487622fb311cce19ea899d49ffe6
Renze Nicolai <renze@rnplus.nl>
2022-05-11 23:38:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ESP32 component: WS2812 LED driver 
+
+ESP32 component for controlling WS2812 or compatible ("Neopixel") addressable LEDs using the RMT peripheral of the ESP32.
+
+Based on code from https://github.com/JSchaenzle/ESP32-NeoPixel-WS2812-RMT.
+
+Licensed using the "Unlicense". This software can be considered public domain.
---
+++ include/ws2812.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdint.h>
+#include <esp_err.h>
+#include <driver/gpio.h>
+
+__BEGIN_DECLS
+
+/**
+ * Initialize the WS2812 driver.
+ * @return ESP_OK on success; any other value indicates an error
+ */
+extern esp_err_t ws2812_init(gpio_num_t aGpioPin);
+
+/**
+ * Enable power to the WS2812 bar.
+ * @return ESP_OK on success; any other value indicates an error
+ */
+extern esp_err_t ws2812_enable(int gpio_pin);
+
+/**
+ * Disable power to the WS2812 bar.
+ * @return ESP_OK on success; any other value indicates an error
+ */
+extern esp_err_t ws2812_disable(void);
+
+/**
+ * Send color-data to the WS2812 bus.
+ * @param data the data-bytes to send on the bus.
+ * @param len the data-length.
+ * @return ESP_OK on success; any other value indicates an error
+ */
+extern esp_err_t ws2812_send_data(uint8_t *data, int len);
+
+__END_DECLS
